16f48a2af3cbbe2e7ec42d65721c8da70000d028
modehans <monicaglezsanch@gmail.com>
2022-11-23 19:24:02 +0100

    Add new styles and create and initialize properties

--- components/eit-counter.js
+++ components/eit-counter.js
@@ -4,13 +4,30 @@ export class EitCounter extends LitElement {
   static styles = [
     css`
       :host {
-        display: block;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+      h2 {
+        color: purple;
       }
     `,
   ];
 
+  static properties = {
+    counter: {
+      type: Number,
+    },
+  };
+
+  constructor() {
+    super();
+    this.counter = 0;
+  }
+
   render() {
-    return html`<h2>Mi contador</h2>`;
+    return html`<h2>Mi contador</h2>
+      <p>Parace que esto funciona bien ${this.counter}</p>`;
   }
 }
 customElements.define('eit-counter', EitCounter);
